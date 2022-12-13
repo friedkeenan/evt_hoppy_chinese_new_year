de6f574fe3f672cfba825e31f61c3f3650c7285d
CaptMaxx <100101520+CaptMaxx@users.noreply.github.com>
2022-12-12 19:57:16 -0500

    Added Translation support

---
+++ files/translations/main.lua
@@ -0,0 +1,70 @@
+local reverseWords = function(text, reversePunctuation)
+	local aux = {}
+	local str = {}
+
+	for word in text:gmatch("%S+") do
+		if reversePunctuation then
+			word = word:gsub("(%P+)(%p+)", "%2%1")
+		end
+		aux[#aux + 1] = word
+	end
+
+	for i=#aux, 1, -1 do
+		str[#str + 1] = aux[i]
+	end
+
+	return table.concat(str, " ")
+end
+
+
+local Text = {}
+local translate
+translate = function(resource, language, gender, _format)
+    language = Text[language] and language or "xx"
+
+    local obj = table.unreference(Text[language])
+    for key in resource:gmatch("%S+") do
+        if type(obj) == "table" then
+            obj = obj[tonumber(key) or key]
+            if not obj then
+                break
+            end
+        else
+            break
+        end
+    end
+
+    if obj then
+		if type(obj) == "table" then
+			if gender then
+				local t = {}
+				
+				for index, val in next, obj do
+					t[index] = (type(val) == "string" and stringutils.getGendered(val, gender) or val)
+				end
+				
+				return t
+			end
+		else
+			if gender and obj:find("%(.-%)") then
+				obj = stringutils.getGendered(obj, gender)
+			end
+			
+			if type(_format) == "table" then
+				for key, value in next, _format do
+					obj = obj:gsub("%$"..key.."%$", tostring(value))
+				end
+			else
+				return tostring(obj)
+			end
+		end
+    else
+        if language ~= "xx" then
+            translate(resource, "xx", gender, _format)
+        else
+            obj = resource:gsub(" ", "%.")
+        end
+    end
+
+    return obj or resource:gsub(" ", "%.")
+end
